9acd9844ae9e82db52fe81548e27bbb94a598172
unknown <Administrateur@PRT-MN-443.marenostrum.local>
2021-10-26 16:40:33 +0200

    react1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect , useState} from "react";
+
+
+
+
+function  Liste(props) {
+    const [pays, setPays] = useState({ hook: [] });
+    const { setData, selectData } = props
+    let index = 0
+
+    function select (e) {
+        const val = e.target.textContent
+        const id = e.target.id
+        const item = document.getElementById(id)
+        let tab = selectData.slice()
+        let found = selectData.includes(val)
+        let remove = (val) =>{
+            const index = tab.indexOf(val);
+            if (index > -1) {
+                tab.splice(index, 1);
+            }
+        }
+        item.classList.contains('selected') ? item.classList.remove('selected') : item.classList.add('selected')
+        found ? remove(val) : tab.push(val)
+        setData(tab)
+
+    }
+
+    useEffect(async function () {
+        const result = await fetch('https://restcountries.com/v3.1/all').then((res) => {
+            return res.json()
+        });
+        setPays(result);
+    }, []);
+    let countryList = Object.keys(pays).map(function(key) {
+        if(pays[key].name){
+            index++
+            const name = pays[key]['name']['common']
+            return   <li id={index} key={name} state={'false'} className={name + ' ' +'item'} onClick={select}> {name} </li>
+        }
+    });
+
+    return <ol className="table"> {countryList} </ol>
+
+}
+
+
+let HashTag = (props) => {
+    const { setData, selectData } = props
+    const option = selectData.map((e)=> {
+        console.log(e)
+        return  <span className={'hash m2'} key={e} value={e}> #{e} </span>
+    })
+    return <div className={'m6 p1 hashTable'}>  {option} </div>
+
+}
+
+
+let  Input = (props) => {
+    const {  selectData } = props
+    if(selectData.length >= 1){
+        return  <div className={'send'}>
+            <input  type={"email"} placeholder={'votre.adresse@mail.com'}/>
+        </div>
+    }else{
+        return null
+    }
+
+}
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [selectData, setData] = useState([])
+
+    return (
+        <div className="App m12">
+            <div className="flexRow m12">
+                <Liste className="m6 p5 " selectData={selectData} setData={setData}/>
+
+                <div className={'flexCol m8'}>
+                    <Input  selectData={selectData} setData={setData}  />
+                    <HashTag selectData={selectData} setData={setData} />
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
